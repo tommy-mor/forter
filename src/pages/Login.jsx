03d85aa41809956706f2612990adffacb9a98dca
Jacob Chvatal <jake@isnt.online>
2022-01-13 20:39:37 -0500

    introduce react use form to login and register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form"
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
@@ -5,6 +6,9 @@ import Button from '@mui/material/Button'
 import Stack from '@mui/material/Stack'
 
 export default function Login() {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const onSubmit = data => console.log(data);
+
   return <Box
     component="form"
     sx={{
@@ -12,20 +16,25 @@ export default function Login() {
     }}
     noValidate
     autoComplete="off"
+    onSubmit={handleSubmit(onSubmit)}
   >
     <Paper sx={{ padding: '1em' }}>
       <Stack alignItems="center" spacing={2}>
         <TextField
           required
+          error={!!errors.username}
           id="outlined-required"
           label="Username"
+          {...register("username", { required: true })}
         />
         <TextField
           required
+          error={!!errors.password}
           id="outlined-required"
           label="Password"
+          {...register("password", { required: true })}
         />
-        <Button variant="contained">
+        <Button variant="contained" type="submit">
           Login
         </Button>
       </Stack>
