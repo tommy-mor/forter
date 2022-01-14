18334980c29ca7af190193dc4b49d4ef9126332f
Jacob Chvatal <jake@isnt.online>
2022-01-13 20:29:22 -0500

    adjust main page style

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -9,31 +9,29 @@ import Button from '@mui/material/Button'
 
 export default function Main() {
   return (
-    <main>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar color="secondary" position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Link component={LocalLink} to="/" >
-                Sorter
-              </Link>
-            </Typography>
-            <Link component={NavLink} to="/users" >
-              <Button color="inherit">Users</Button>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar color="secondary" position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link component={LocalLink} to="/" >
+              Sorter
             </Link>
-            <Link component={NavLink} to="/tags" >
-              <Button color="inherit">Tags</Button>
-            </Link>
-            <Link component={NavLink} to="/login" >
-              <Button color="inherit">Login</Button>
-            </Link>
-            <Link component={NavLink} to="/register" >
-              <Button color="inherit">Register</Button>
-            </Link>
-          </Toolbar>
-        </AppBar>
-      </Box>
+          </Typography>
+          <Link component={NavLink} to="/users" >
+            <Button color="inherit">Users</Button>
+          </Link>
+          <Link component={NavLink} to="/tags" >
+            <Button color="inherit">Tags</Button>
+          </Link>
+          <Link component={NavLink} to="/login" >
+            <Button color="inherit">Login</Button>
+          </Link>
+          <Link component={NavLink} to="/register" >
+            <Button color="inherit">Register</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
       <Outlet />
-    </main>
+    </Box>
   )
 }
