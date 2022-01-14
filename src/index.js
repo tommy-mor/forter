60ebe4f3fd712b611c8117d50b1edc053922539d
Jacob Chvatal <jake@isnt.online>
2022-01-13 20:29:11 -0500

    add theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from '@mui/material/styles'
+
 import App from './routes'
+import theme from './theme'
 import reportWebVitals from './reportWebVitals'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles'
+
+export default createTheme({
+  palette: {
+    primary: {
+      main: '#0ff',
+    },
+    secondary: {
+      main: '#ffc0cb',
+    },
+  },
+});
