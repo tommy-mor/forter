7fa70b6fa05b08f989fdbdf10fb5aa0429f45c75
Jacob Chvatal <jake@isnt.online>
2022-01-13 19:25:50 -0500

    toolbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function Home() {
   return <div>This is Home, not much here yet</div>
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,35 @@
 import { NavLink, Outlet } from 'react-router-dom';
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 export default function Main() {
   return (
     <main>
-      <nav>
-        <NavLink to="/users" > Users </NavLink>
-        <NavLink to="/tags" > Tags </NavLink>
-        <NavLink to="/login" > Login </NavLink>
-        <NavLink to="/register" > Register </NavLink>
-      </nav>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Sorter
+            </Typography>
+            <NavLink to="/users" >
+              <Button color="inherit">Users</Button>
+            </NavLink>
+            <NavLink to="/Tags" >
+              <Button color="inherit">Tags</Button>
+            </NavLink>
+            <NavLink to="/login" >
+              <Button color="inherit">Login</Button>
+            </NavLink>
+            <NavLink to="/register" >
+              <Button color="inherit">Register</Button>
+            </NavLink>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <Outlet />
     </main>
   )
